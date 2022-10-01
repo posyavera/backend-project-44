a91a91e3473a1da253ba4234d11bc1b742393590
compositeMaterial <elanakov.v@gmail.com>
2022-10-01 13:49:56 +0700

    Import

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
+import { askName } from '../src/cli.js'
 console.log('Welcome to the Brain Games!');
-import askingName  from "../src/cli.js";
-const name = askingName();
+askName();
