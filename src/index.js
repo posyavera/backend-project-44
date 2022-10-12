565eb04de181e797fea47714fbc370f1d7dc2449
compositeMaterial <elanakov.v@gmail.com>
2022-10-12 22:22:26 +0700

    reverted commit  eaef2ef8fec400f582fcd5486668bd3a36060082

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const logicFunction = (question, task) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${name} !`);
+  console.log(`Hello, ${name} !`);
   for (let i = 0; i < 3; i += 1) {
     console.log(question);
     const questionResult = task();
